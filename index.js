28f7071a4adcf5c568e7246e87995b8e3be1a49d
VladyslavaGoit <116902393+VladyslavaGoit@users.noreply.github.com>
2023-02-17 21:16:10 +0200

    test3

--- index.js
+++ index.js
@@ -23,10 +23,20 @@
 //число (кількість хвилин) і виведе в консоль
 //рядок у форматі годин і хвилин, тобто 70 має повернути у консоль "01:10"
 
-const value = Number(prompt("Enter the number of minutes to format"));
+// const value = Number(prompt("Enter the number of minutes to format"));
 
-const hours = String(Math.floor(value / 60)).padStart(2, "0");
+// const hours = String(Math.floor(value / 60)).padStart(2, "0");
 
-const minutes = String(value % 60).padStart(2, "0");
+// const minutes = String(value % 60).padStart(2, "0");
 
-console.log(`${hours}:${minutes}`);
+// console.log(`${hours}:${minutes}`);
+//---------------------------------------
+//3. Напишіть цикл, який виведе в консоль усі парні числа від max до min включно по зменшенню
+// const min = 1;
+// const max = 12;
+// for (let index = max; index >= min; index -= 1) {
+//     if (!(index % 2)) {
+//         console.log(index)
+//     } 
+    
+// }
